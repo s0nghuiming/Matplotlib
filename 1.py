2babe1d96d7cca8c2d6bee45e8c0f55a4e97f181
Song, HuimingX <huimingx.song@intel.com>
2020-01-02 17:50:18 +0800

    Init

---
+++ 1.py
@@ -0,0 +1,10 @@
+# sphinx_gallery_thumbnail_number = 3
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()  # an empty figure with no axes
+fig.suptitle('No axes on this figure')  # Add a title so we know which it is
+
+fig, ax_lst = plt.subplots(2, 2)  # a figure with a 2x2 grid of Axes
+
+plt.show()
